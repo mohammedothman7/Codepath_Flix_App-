9e45b3a1ad17d7a8141a5925541fbbd7c08ffe44
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2020-09-23 21:05:48 -0400

    finished flix part 2

---
+++ Codepath_Flix_App/Codepath_Flix_App/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Codepath_Flix_App
+//
+//  Created by Mohammed Othman on 9/23/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+   
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
--- Codepath_Flix_App/Codepath_Flix_App/MoviesViewController.swift
+++ Codepath_Flix_App/Codepath_Flix_App/MoviesViewController.swift
@@ -72,14 +72,22 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
       }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+//     MARK: - Navigation
+//
+//     In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+//         Get the new view controller using segue.destination.
+//         Pass the selected object to the new view controller.
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
+    
 
 }
